d9874254258df18b7638fc0c4c700e58bed4724e
Htet Naing Aung <justin.aung19@gmail.com>
2016-11-19 20:16:52 +0700

    Detect if we hit a physics object

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -40,27 +40,38 @@ void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompon
       OUT PlayerViewPointRotation
    );
    
-   // Log out ot test
-//   UE_LOG(LogTemp, Display, TEXT("Player | Location : %s, Rotation : %s"),
-//      *PlayerViewPointLocation.ToString(),
-//      *PlayerViewPointRotation.ToString()
-//   );
-   
    FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
    
    // Draw a red trace in the world to visualize
-   DrawDebugLine(GetWorld(),
-                 PlayerViewPointLocation,
-                 LineTraceEnd,
-                 FColor(255, 0, 0),
-                 false,
-                 0.f,
-                 0.f,
-                 10.f);
+   DrawDebugLine(
+      GetWorld(),
+      PlayerViewPointLocation,
+      LineTraceEnd,
+      FColor(255, 0, 0),
+      false,
+      0.f,
+      0.f,
+      10.f
+   );
    
-   // Ray-cast out to reach distance
+   // Setup query parameters
+   FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+   
+   // Line-trace (AKA ray-cast) out to reach distance
+   FHitResult Hit;
+   GetWorld()->LineTraceSingleByObjectType(
+      OUT Hit,
+      PlayerViewPointLocation,
+      LineTraceEnd,
+      FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+      TraceParameters
+   );
    
    // See what we hit
-   
+   AActor* ActorHit = Hit.GetActor();
+   if (ActorHit)
+   {
+      UE_LOG(LogTemp, Warning, TEXT("Line trace hit: %s"), *ActorHit->GetName());
+   }
 }
 
